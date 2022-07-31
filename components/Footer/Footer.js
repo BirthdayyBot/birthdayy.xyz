0948fcdfb5513de8cb25412d25f8d0b4b8e4c340
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-07-31 17:45:28 +0200

    Fixed Footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ import style from "../../styles/Footer.module.scss";
 
 const Footer = () => {
   return (
-    <div className="footer_component mt-3">
+    <div className={style.footer_component}>
       <div className="content has-text-centered">
         <p>Nikolai Schunk - 2022</p>
         <Link href="/disclosure">Legal Disclosure</Link>
