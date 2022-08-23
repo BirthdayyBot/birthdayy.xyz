d54a3d0de3c4d403176f75e4d5c2d49bf9700a49
nikolaischunk <nikolai.schunk@swisscom.com>
2022-08-23 09:13:56 +0200

    Added Status to the Navbar

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
         permanent: true,
       },
       {
+        source: "/docs",
+        destination: "https://docs.birthdayy.xyz/",
+        permanent: true,
+      },
+      {
+        source: "/status",
+        destination: "https://status.birthdayy.xyz/",
+        permanent: true,
+      },
+      {
         source: "/invite",
         destination:
           "https://discord.com/oauth2/authorize?client_id=916434908728164372&permissions=525529836753&scope=applications.commands%20bot",
