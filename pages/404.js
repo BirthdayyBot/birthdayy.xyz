1c9a81492e52f4550fcbb3ea432a975070b221ac
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-09-16 14:33:00 +0200

    Added buttons and 404 text

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,23 @@
 const Error = () => {
   return (
     <div className="error container">
-      <h1 className="title is-centered">404</h1>
+      <h1 className="is-title">
+        It seems like this Page does not exist :(
+      </h1>
+      <div className="columns mt-5">
+        <div className="column is-3">
+          <button className="button is-primary is-large is-fullwidth is-rounded is-outlined">
+            Return Home
+          </button>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column is-3">
+          <button className="button is-primary is-large is-fullwidth is-rounded">
+            Invite Birthday
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
