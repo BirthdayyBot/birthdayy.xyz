32f97c07ac1c62e71a56e83e7e9798171e48cf59
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-09-16 14:49:07 +0200

    Added Links to button

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,20 @@
+import Link from "../components/Generic/Link";
+
 const Error = () => {
   return (
     <div className="error container">
-      <h1 className="is-title">
-        It seems like this Page does not exist :(
-      </h1>
+      <h1 className="is-title">It seems like this Page does not exist :(</h1>
       <div className="columns mt-5">
         <div className="column is-3">
           <button className="button is-primary is-large is-fullwidth is-rounded is-outlined">
-            Return Home
+            <Link url="/"> Return Home </Link>
           </button>
         </div>
       </div>
       <div className="columns">
         <div className="column is-3">
           <button className="button is-primary is-large is-fullwidth is-rounded">
-            Invite Birthday
+            <Link url="/invite">Invite Birthday</Link>
           </button>
         </div>
       </div>
