48cd9bd103884faf0fc63d665089059248f7cd0c
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-09-25 20:39:33 +0200

    Added Feature Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import IndexComponent from "../components/Homepage";
-
+import FeatureComponent from "../components/Homepage/Feature";
 
 export default function Index() {
   return (
     <div className="index">
       <IndexComponent />
+      <FeatureComponent />
     </div>
   );
 }
