6e671e17c256e2c4ee0e08acbb25493047320b36
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-09-29 18:05:18 +0200

    Tryout Feature Style

--- components/Homepage/Feature.js
+++ components/Homepage/Feature.js
@@ -8,36 +8,87 @@ const FeatureComponent = () => {
   return (
     <div className="feature_component container mt-5">
       <div className={`${style.spacer}`}></div>
-      <div className="section">
-        <h1 className="is-title has-text-centered">Features</h1>
+      {/* <div className="section"> */}
+      <h1 className="is-title has-text-centered">Features</h1>
+      <div className="columns is-centered">
+        <div className="column is-6">
+          <div className="tags is-centered">
+            <span className="tag is-primary">Seven</span>
+            <span className="tag is-primary">Seven</span>
+            <span className="tag is-primary">Seven</span>
+            <span className="tag is-primary">Seven</span>
 
-        <div className="columns mt-5">
-          <div className="column is-1"></div>
+            <span className="tag is-dark">Seven</span>
+            <span className="tag is-dark">Seven</span>
+            <span className="tag is-dark">Seven</span>
+            <span className="tag is-dark">Seven</span>
 
-          <div className="column is-5">
-            <div>
-              <Image
-                src="https://bulma.io/images/placeholders/640x480.png"
-                width={640}
-                height={480}
-                alt="Placeholder image"
-                className={`${style.showcase_image}`}
-              />
-            </div>
+            <span className="tag is-black">Black</span>
+            <span className="tag is-dark">Dark</span>
+            <span className="tag is-light">Light</span>
+            <span className="tag is-white">White</span>
+            <span className="tag is-primary">Primary</span>
+            <span className="tag is-link">Link</span>
+            <span className="tag is-info">Info</span>
+            <span className="tag is-success">Success</span>
+            <span className="tag is-warning">Warning</span>
+            <span className="tag is-danger">Danger</span>
           </div>
-          <Divider text={""} color="dark" isVertical />
-          <div className="column is-5">
-            <h3 className="block is-title is-size-3">
-              <IconText text={"Hello World"} icon={<ArrowRight />} />
-            </h3>
-            {/* <h3 className="block is-title is-size-3">Hello World</h3> */}
-            <p className="block is-text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Reiciendis soluta <IconText text={"abc"} icon={<ArrowRight />} />
-              voluptatibus autem molestias vel fuga voluptatem nostrum dicta
-              ducimus quibusdam porro nam tenetur, dolore veritatis magni, illum
-              quae quas harum.
-            </p>
+        </div>
+      </div>
+
+      <div className="columns mt-5 feature_component is-centered">
+        <div className="column is-5">
+          <div>
+            <Image
+              src="/media/images/features/announcement.webp"
+              width={1920}
+              height={1680}
+              alt="Placeholder image"
+              className={`${style.showcase_image}`}
+            />
+          </div>
+        </div>
+        {/* <Divider text={""} color="dark" isVertical /> */}
+        {/* <IconText text={"Hello World"} icon={<ArrowRight />} /> */}
+        <div className="column is-3">
+          <h3 className="block is-title is-size-3 mt-5">
+            Birthday Announcement
+          </h3>
+          <p className="block is-sub-title">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
+            soluta <IconText text={"abc"} icon={<ArrowRight />} />
+            voluptatibus autem molestias vel fuga voluptatem nostrum dicta
+            ducimus quibusdam porro nam tenetur, dolore veritatis magni, illum
+            quae quas harum.
+          </p>
+        </div>
+      </div>
+
+      <div className="columns mt-5 feature_component is-centered">
+        {/* <Divider text={""} color="dark" isVertical /> */}
+        {/* <IconText text={"Hello World"} icon={<ArrowRight />} /> */}
+        <div className="column is-3">
+          <h3 className="block is-title is-size-3 mt-5">
+            Birthday Announcement
+          </h3>
+          <p className="block is-sub-title">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
+            soluta <IconText text={"abc"} icon={<ArrowRight />} />
+            voluptatibus autem molestias vel fuga voluptatem nostrum dicta
+            ducimus quibusdam porro nam tenetur, dolore veritatis magni, illum
+            quae quas harum.
+          </p>
+        </div>
+        <div className="column is-5">
+          <div>
+            <Image
+              src="/media/images/features/announcement.webp"
+              width={1920}
+              height={1680}
+              alt="Placeholder image"
+              className={`${style.showcase_image}`}
+            />
           </div>
         </div>
       </div>
