5573141f082dafd84f2fb8b0af556f8bc105e6d4
Nikolai Schunk <58025175+nikolaischunk@users.noreply.github.com>
2024-08-18 21:17:22 +0200

    feat: always use correct year

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className={style.footer_component}>
       <div className={`${style.footer_content} has-text-centered`}>
-        <p>Birthdayy - 2022</p>
+        <p>Birthdayy - {new Date().getFullYear()}</p>
         <Link href="/disclosure">
           <a className="is-text has-text-white">Legal Disclosure</a>
         </Link>
